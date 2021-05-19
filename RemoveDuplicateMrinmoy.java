85aedcdb3e6109a12c5da189f1a9306c4135e7b2
Satyajit <gautam.satyajit@gmail.com>
2021-05-20 01:55:43 +0530

    xyz

--- RemoveDuplicateMrinmoy.java
+++ RemoveDuplicateMrinmoy.java
@@ -5,22 +5,7 @@ import java.util.HashMap;
 public class RemoveDuplicateMrinmoy {
 	
 	public static void main(String[] args) {
-		int[] intArray = new int[]{ 2 };
-
-		
-		HashMap<Integer, String> map = new HashMap<Integer, String>(); 
-		
-//		map.put(intArray[0], "");
-		for (int i=1;i<intArray.length; i++ ) {
-			if(!(map.containsKey(intArray[i]))) {
-				map.put(intArray[i], "");
-			}
-		}
-		
-		for(int kk : map.keySet()) {
-			System.out.println(kk);
-		}
-		
+		System.out.println("ABCD");
 	}
 
 }
